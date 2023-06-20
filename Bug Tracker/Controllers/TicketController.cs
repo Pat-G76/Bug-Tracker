274f50910b4bd116191564bec8b67bdc6a190547
Patrick <patrickinvader@outlook.com>
2023-06-20 12:59:13 +0200

    create ticket working

--- Bug Tracker/Controllers/TicketController.cs
+++ Bug Tracker/Controllers/TicketController.cs
@@ -1,8 +1,10 @@
 ﻿using Bug_Tracker.Data;
 using Bug_Tracker.Models;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
 
 namespace Bug_Tracker.Controllers
 {
@@ -26,33 +28,51 @@ namespace Bug_Tracker.Controllers
         }
 
 
-        public IActionResult CreateTicket(int id)
+        public async Task<IActionResult> CreateTicket(int id)
         {
+
+			Employee employee = await userManager.FindByNameAsync(User.Identity.Name);
+			Ticket ticket = new Ticket();
+
+			ticket.ProjectID = id;
+			ticket.EmployeeId = employee.Id;
+
+
+            if (wrapper.Project.GetById(id) == null)
+            {
+				return NotFound();
+			}
+            else if( !await userManager.IsInRoleAsync(employee, "administrator") && !IsInProject(employee, id) )
+            {
+				return NotFound();
+			}            
+
 
 			ViewBag.Changes = "Create";
 			ViewBag.Priorities = wrapper.Priority.GetAllItems();
             ViewBag.IssueTypes = wrapper.IssueType.GetAllItems();
 
-            Ticket ticket = new Ticket();
-
-            ticket.ProjectID = id;
 
             return View(ticket);
         }
 
-		public IActionResult UpdateTicket(int id)
+		public async Task<IActionResult> UpdateTicket(int id)
 		{
 
-
             Ticket ticket = wrapper.Ticket.GetById(id);
+			Employee employee = await userManager.FindByNameAsync(User.Identity.Name);
 
-            if(ticket == null)
+			if (ticket == null)
             {
                 return NotFound();
             }
+            else if (!await userManager.IsInRoleAsync(employee, "administrator") && !IsInProject(employee, id))
+			{
+				return NotFound();
+			}
 
 
-            ViewBag.Changes = "Update";
+			ViewBag.Changes = "Update";
 			ViewBag.Priorities = wrapper.Priority.GetAllItems();
 			ViewBag.IssueTypes = wrapper.IssueType.GetAllItems();
 
@@ -68,24 +88,62 @@ namespace Bug_Tracker.Controllers
                 ModelState.AddModelError("", "You cannot add a due date that already passed");
 
 
-            if(ModelState.IsValid)
+			Employee assignee = string.IsNullOrEmpty(ticket.AssigneeFirstName) && string.IsNullOrEmpty(ticket.AssigneeLastName) ? null : userManager.Users.FirstOrDefault(e => e.FirstName.ToLower() == ticket.AssigneeFirstName.ToLower() && e.LastName.ToLower() == ticket.AssigneeLastName.ToLower());
+
+			if (assignee != null)
             {
 
-                if(ticket.TicketID == 0)
+                if(!IsInProject(assignee, ticket.ProjectID))
                 {
+                    ModelState.AddModelError("", "The assignee is not in the project. Talk to the administrator");
+                }                
+
+			}
+			else
+            {
+                ModelState.AddModelError("", "Assignee FirstName and/or LastName do not match");
+            }
+
+
+			if (ModelState.IsValid)
+            {
+                
+                if (ticket.TicketID == 0)
+                 {
 
                     ticket.PriorityID = chosenPriority;
                     ticket.IssueTypeID = chosenIssueType;
                     ticket.Created = DateTime.Now;
                     ticket.StatusID = wrapper.Status.GetByTitle("New").StatusID;
 
+                    wrapper.Ticket.Add(ticket);
+                    wrapper.saveChanges();
+
+                    
                 }
                 else
                 {
 
-                }
+                    Ticket oldTicket = wrapper.Ticket.GetById(ticket.TicketID);
 
-            }
+                    oldTicket.TicketName = ticket.TicketName;
+                    oldTicket.TicketDescription = ticket.TicketDescription;
+                    oldTicket.AssigneeFirstName = ticket.AssigneeFirstName;
+                    oldTicket.AssigneeLastName = ticket.AssigneeLastName;
+                    
+					oldTicket.PriorityID = chosenPriority;
+					oldTicket.IssueTypeID = chosenIssueType;										
+
+					wrapper.Ticket.Update(oldTicket);
+					wrapper.saveChanges();
+
+				}
+
+				return RedirectToAction("Index");
+
+			}
+
+            ViewBag.Changes = ticket.TicketID == 0 ? "Create" : "Update";
 
 			ViewBag.Priorities = wrapper.Priority.GetAllItems();
 			ViewBag.IssueTypes = wrapper.IssueType.GetAllItems();
@@ -95,6 +153,15 @@ namespace Bug_Tracker.Controllers
 
         }
 
+        private bool IsInProject(Employee employee, int projectID)
+		{
+
+			IEnumerable<ProjectEmployee> projectEmployees = wrapper.ProjectEmployee.GetAllItems().Where(pe => pe.ProjectID == projectID);
+
+            return projectEmployees.FirstOrDefault(pe => pe.EmployeeId == employee.Id) != null;
+    
+
+		}
 
 	}
 }
