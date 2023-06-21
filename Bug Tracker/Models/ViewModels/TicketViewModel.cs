a7e3bb5dcd621926c8566dbee7345d8abcd00c2a
Patrick <patrickinvader@outlook.com>
2023-06-21 14:29:04 +0200

    tickets table layout

--- Bug Tracker/Models/ViewModels/TicketViewModel.cs
+++ Bug Tracker/Models/ViewModels/TicketViewModel.cs
@@ -9,5 +9,23 @@
 		public IEnumerable<IssueType> issueTypes { get; set; }
 		public Project project { get; set; }
 
+
+		public string GetColor(int priorityID)
+		{
+
+			switch(priorityID)
+			{
+
+				case 5: return "#FF0000"; 
+				case 4: return "#FFA500";
+				case 3: return "#FFFF00";
+				case 2: return "#00FF00";
+				case 1: return "#0000ff";
+				default: return "#ffffff";
+
+			}
+		
+		}
+
 	}
 }
